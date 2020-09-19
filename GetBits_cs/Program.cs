7110d6d71816846e92cfbcff1833230fcb3c4611
ahidaka <hidaka@devdrv.co.jp>
2020-09-19 23:34:36 +0900

    add  GetBits_cs

---
+++ GetBits_cs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace GetBits_cs
+{
+
+    public class BitArray
+    {
+        const int SZ = 8;
+        const ulong SEVEN = 7;
+
+        public ulong GetBits(byte[] inArray, uint start, uint length)
+        {
+            ulong ul = 0;
+            uint startBit = (uint)start % SZ;
+            uint startByte = (uint)start / SZ;
+            //BYTE* pb = &inArray[startByte];
+            uint posInArray = startByte;
+            ulong dataInArray;
+            uint i;
+            uint pos;
+
+            pos = startBit;
+            for (i = 0; i < length; i++)
+            {
+                ul <<= 1;
+                dataInArray = inArray[posInArray];
+                ul |= (dataInArray >> (int)(SEVEN - pos++)) & 1;
+                if (pos >= SZ)
+                {
+                    pos = 0;
+                    dataInArray++;
+                }
+            }
+            return ul;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] sampleArray = new byte[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int i;
+            uint start = 3;
+            uint length = 6;
+            BitArray ba = new BitArray();
+
+            Console.WriteLine("Hello Bits World!");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("0x{0:X} ", sampleArray[i]);
+            }
+            Console.WriteLine("");
+
+            for (i = 0; i < 10; i++)
+            {
+                ulong answer = ba.GetBits(sampleArray, start, length);
+
+                Console.WriteLine("Bits i:{0} start:{1} len:{2} ans:{3},0x{3:X}",
+                    i, start, length, answer);
+
+                start += 7;
+                length++;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is basic method for Common Model. Just think about it.
 
 Maybe someone knows this should be a basic and important code to implement "Common Model".
 What is "Common Model" ? God Only Knows.
+
+Special service! We added CSharp/dotnet core version of this. Enjoy.
